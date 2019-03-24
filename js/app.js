24a8293cf016de9325883e5d49389d13e42bba2d
AtomCode <mr.ali.alhelal@gmail.com>
2019-03-24 14:53:44 +0300

    Function Done + Working in the Desgin

--- js/app.js
+++ js/app.js
@@ -12,19 +12,112 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
+
+function startGame(cardslist)
+{
+
+  var cardslist = [
+   'fa fa-diamond','fa fa-diamond',
+   'fa fa-cube','fa fa-cube',
+   'fa fa-paper-plane-o','fa fa-paper-plane-o',
+   'fa fa-bicycle','fa fa-bicycle',
+   'fa fa-bolt','fa fa-bolt',
+   'fa fa-bomb','fa fa-bomb',
+   'fa fa-leaf','fa fa-leaf',
+   'fa fa-anchor','fa fa-anchor',
+  ];
+
+// using shuffle function to shuffle all cards
+cardslist = shuffle(cardslist);
+
+let deck = document.querySelector(".deck");
+cardslist.forEach(function(card)
+{
+   onecardset='<li class="card"><i class="'+ card +'"></i></li>';
+   deck.insertAdjacentHTML('afterbegin', onecardset);
+});
+
+}
+startGame();
+
+// Select All cards and Add EventListener to theme so when action onclick, card will show up
+let cards = document.querySelectorAll(".card");
+let moves = 0;
+var showencards = [];
+
+
+$( document ).ready(function() {
+  $( ".card" ).click(function() {
+    moves++;
+    $(".moves").text(moves);
+    showencards.push( $(this).children("i").attr("class") );
+
+  $(this).addClass('open show');
+
+    if(showencards.length==2)
+    {
+      $(".deck").css("pointer-events", "none");
+  //do something
+
+
+        if(showencards[0] == showencards[1])
+        {
+          // alert("==");
+          $(".deck").find(".open.show").addClass("match");
+          $(".deck").find(".open.show").removeClass("open show");
+
+        }
+        else
+        {
+          // alert("!!");
+           setTimeout(function(){
+            $(".deck").find(".open.show").removeClass("open show");
+             $('.stars li').first().remove(); }, 1000);
+
+        }
+        setTimeout(function()
+        {
+            check();
+            showencards = [];
+            $(".deck").css("pointer-events", "auto");
+         }, 1000);
+
+
+
+    }
+
+    // alert( "Handler for .click() called."+     $(this).children("i").attr("class") );
+  });
+
+
+  $( ".restart" ).click(function() {
+     location.reload();
+  });
+
+
+  function check()
+  {
+
+
+  if($(".match").length == cards.length ){alert("Good Job You did it!");}
+  }
+
+
+});
 
 /*
  * set up the event listener for a card. If a card is clicked:
