13b9a485b546bce6471a99c60b0b5c0563d584ea
AtomCode <mr.ali.alhelal@gmail.com>
2019-03-26 04:48:58 +0300

    Timer , Star , Match , moves are Done , need to work in  ending game + responsive for smart phone

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 /*
- * Create a list that holds all of your cards
+ * Create a divst that holds all of your cards
  */
 
 
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - shuffle the divst of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
@@ -26,106 +26,154 @@ function shuffle(array) {
   return array;
 }
 
+// This array Contain divst of cards used on the game, there is 8 diffret shape each shape repeated twice
+let cardlist = [
+  'fa fa-diamond', 'fa fa-diamond',
+  'fa fa-cube', 'fa fa-cube',
+  'fa fa-paper-plane-o', 'fa fa-paper-plane-o',
+  'fa fa-bicycle', 'fa fa-bicycle',
+  'fa fa-bolt', 'fa fa-bolt',
+  'fa fa-bomb', 'fa fa-bomb',
+  'fa fa-leaf', 'fa fa-leaf',
+  'fa fa-anchor', 'fa fa-anchor',
+];
 
-function startGame(cardslist)
-{
 
-  var cardslist = [
-   'fa fa-diamond','fa fa-diamond',
-   'fa fa-cube','fa fa-cube',
-   'fa fa-paper-plane-o','fa fa-paper-plane-o',
-   'fa fa-bicycle','fa fa-bicycle',
-   'fa fa-bolt','fa fa-bolt',
-   'fa fa-bomb','fa fa-bomb',
-   'fa fa-leaf','fa fa-leaf',
-   'fa fa-anchor','fa fa-anchor',
-  ];
+function GenerateCard(cards) {
 
-// using shuffle function to shuffle all cards
-cardslist = shuffle(cardslist);
 
-let deck = document.querySelector(".deck");
-cardslist.forEach(function(card)
-{
-   onecardset='<li class="card"><i class="'+ card +'"></i></li>';
-   deck.insertAdjacentHTML('afterbegin', onecardset);
-});
+  let result = null;
+  result = '<div class="row">';
+  result = result + '<div><div class="card">  <i class="' + cards[0] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[1] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[2] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[3] + '"></i>  </div>   </div>';
+  result = result + '</div>';
+  result = result + '<div class="row">';
+  result = result + '<div><div class="card">  <i class="' + cards[4] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[5] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[6] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[7] + '"></i>  </div>   </div>';
+  result = result + '</div>';
+  result = result + '<div class="row">';
+
+  result = result + '<div><div class="card">  <i class="' + cards[8] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[9] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[10] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[11] + '"></i>  </div>   </div>';
+  result = result + '</div>';
+  result = result + '<div class="row">';
+  result = result + '<div><div class="card">  <i class="' + cards[12] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[13] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[14] + '"></i>  </div>   </div>';
+  result = result + '<div><div class="card">  <i class="' + cards[15] + '"></i>  </div>   </div>';
+  result = result + '</div>';
+
+  return result;
+}
+
+// startGame function will generate the cards in the deck  also shuffle the card and works as Refresh
+function startGame() {
+
+  $(function() {
+
+    let result = GenerateCard(cardlist);
+
+    $('.deck').children().remove();
+    $('.deck').append(result);
+
+    // countDownTo value in milliseconds , 1000 = 1 sec
+    let countDownTo = 120000;
+
+    // Time calculations for remaining minutes and seconds
+    let minutes = Math.floor((countDownTo % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((countDownTo % (1000 * 60)) / 1000);
+
+    //Display Time for the user
+    $("#Timer").text(minutes + ":" + seconds);
+
+    //Start Timer
+    var timer = setInterval(setTimer, 1000);
+
+    function setTimer()
+    {
+
+      countDownTo = countDownTo - 1000;
+
+      // Re calculate Time for remaining minutes and seconds
+      minutes = Math.floor((countDownTo % (1000 * 60 * 60)) / (1000 * 60));
+      seconds = Math.floor((countDownTo % (1000 * 60)) / 1000);
+
+      $("#Timer").text(minutes + ":" + seconds)
+      if (countDownTo == 0)
+        clearInterval(timer);
+    }
+
+
+
+  });
+
 
 }
 startGame();
 
-// Select All cards and Add EventListener to theme so when action onclick, card will show up
+
+// To get all cards class
 let cards = document.querySelectorAll(".card");
+// number of moves in the games
 let moves = 0;
+// number of showen cards, max 2 cards
 var showencards = [];
 
-
-$( document ).ready(function() {
-  $( ".card" ).click(function() {
+$(document).ready(function() {
+  $(".card").click(function() {
     moves++;
     $(".moves").text(moves);
-    showencards.push( $(this).children("i").attr("class") );
+    showencards.push($(this).children("i").attr("class"));
 
-  $(this).addClass('open show');
+    $(this).addClass('open show');
 
-    if(showencards.length==2)
-    {
+    if (showencards.length == 2) {
       $(".deck").css("pointer-events", "none");
-  //do something
 
+      if (showencards[0] == showencards[1]) {
+        // alert("==");
+        $(".deck").find(".open.show").addClass("match");
+        $(".deck").find(".open.show").removeClass("open show");
 
-        if(showencards[0] == showencards[1])
-        {
-          // alert("==");
-          $(".deck").find(".open.show").addClass("match");
+      } else {
+
+        setTimeout(function() {
           $(".deck").find(".open.show").removeClass("open show");
+          $('.stars li').first().remove();
+        }, 1000);
 
-        }
-        else
-        {
-          // alert("!!");
-           setTimeout(function(){
-            $(".deck").find(".open.show").removeClass("open show");
-             $('.stars li').first().remove(); }, 1000);
-
-        }
-        setTimeout(function()
-        {
-            check();
-            showencards = [];
-            $(".deck").css("pointer-events", "auto");
-         }, 1000);
+      }
+      setTimeout(function() {
+        check();
+        showencards = [];
+        $(".deck").css("pointer-events", "auto");
+      }, 1000);
 
 
 
     }
 
-    // alert( "Handler for .click() called."+     $(this).children("i").attr("class") );
+    // alert( "Handler for .cdivck() called."+     $(this).children("i").attr("class") );
   });
 
 
-  $( ".restart" ).click(function() {
-     location.reload();
+  $(".restart").cdivck(function() {
+    location.reload();
   });
 
 
-  function check()
-  {
-
-
-  if($(".match").length == cards.length ){alert("Good Job You did it!");}
+  function check() {
+    if ($(".match").length == 16) {
+      alert("Good Job You did it!");clearInterval(timer);
+    }
   }
 
 
-});
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+});
